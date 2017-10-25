3bcc3b9b907f5be3a3b7d5bda00c5b29439338eb
m.jarembinsky <marian.jarembinsky@codexa.sk>
2017-10-25 15:26:28 +0200

    Initial commit

---
+++ src/main/java/sk/flowy/cashregisterservice/CashRegisterServiceApplication.java
@@ -0,0 +1,12 @@
+package sk.flowy.cashregisterservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashRegisterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashRegisterServiceApplication.class, args);
+	}
+}
